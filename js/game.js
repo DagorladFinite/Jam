364ee7ede9ee0b6edb3acc85ff01f4d9f5dab271
dew-x <afigueras@enti.cat>
2015-10-19 16:07:49 +0200

    v0

--- js/game.js
+++ js/game.js
@@ -1,7 +1,16 @@
 function Game(canvasID) {
     this.canvasID = canvasID;
+    this.ctx = document.getElementById(this.canvasID).getContext("2d");
 }
 
 Game.prototype.init = function () {
-    
+    window.requestAnimationFrame(this.update.bind(this))
+}
+
+Game.prototype.draw = function () {
+
+}
+
+Game.prototype.update = function (timestamp) {
+    console.log(this);
 }
