65a09c30b9afc358446aa042509326557621b22f
dew-x <afigueras@enti.cat>
2015-10-19 16:33:50 +0200

    added loading bar

--- js/game.js
+++ js/game.js
@@ -5,9 +5,20 @@ function Game(canvasID) {
     this.scene = "loading";
     this.width = 800;
     this.height = 600;
+    this.imageList = [];
+    this.img = {};
+    this.loaded = 0;
 }
 
 Game.prototype.init = function () {
+    for (var i=0; i<this.imageList.length; ++i) {
+        if (!this.img.hasOwnProperty(this.imageList[i])) {
+            this.img[this.imageList[i]]={
+                img: new Image(),
+            }
+            this.img[this.imageList[i]].img.src = this.imageList[i];
+        }
+    }
     window.requestAnimationFrame(this.update.bind(this));
 }
 
@@ -15,6 +26,15 @@ Game.prototype.draw = function () {
     if (this.scene == "loading") {
         this.ctx.fillStyle="white";
         this.ctx.fillRect(0,0,this.width, this.height);
+        this.ctx.fillStyle="black";
+        var x0 = this.width * 0.3;
+        var w = this.width*0.4;
+        var y0 = this.height*0.8;
+        var h = this.height*0.05;
+        this.ctx.fillRect(x0,y0,w,h);
+        this.ctx.fillStyle="red";
+        var border = 2;
+        this.ctx.fillRect(x0+border,y0+border,(w-border*2)*this.loaded, h-border*2);
     }
 }
 
@@ -26,7 +46,16 @@ Game.prototype.update = function (timestamp) {
 
 Game.prototype.updateTime = function (timestamp) {
     if (this.scene=="loading") {
-
+        var done=0;
+        for (var key in this.img) {
+            if (this.img.hasOwnProperty(key) && this.img[key].img.completed) ++done; 
+        }
+        var count = Object.keys(this.img).length;
+        if (count==0) {
+            this.loaded = 1;
+        } else {
+            this.loaded = done/count; 
+        }
     } else {
         if (this.last!=null) {
             var delta = timestamp - this.last
