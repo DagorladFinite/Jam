f36818f84eba130b00a95e19054f0a94539bbf8d
dew-x <afigueras@enti.cat>
2015-10-19 16:19:37 +0200

    added loading

--- js/game.js
+++ js/game.js
@@ -1,16 +1,36 @@
 function Game(canvasID) {
     this.canvasID = canvasID;
     this.ctx = document.getElementById(this.canvasID).getContext("2d");
+    this.last = null;
+    this.scene = "loading";
+    this.width = 800;
+    this.height = 600;
 }
 
 Game.prototype.init = function () {
-    window.requestAnimationFrame(this.update.bind(this))
+    window.requestAnimationFrame(this.update.bind(this));
 }
 
 Game.prototype.draw = function () {
-
+    if (this.scene == "loading") {
+        this.ctx.fillStyle="white";
+        this.ctx.fillRect(0,0,this.width, this.height);
+    }
 }
 
 Game.prototype.update = function (timestamp) {
-    console.log(this);
+    this.updateTime(timestamp);
+    this.draw();
+    window.requestAnimationFrame(this.update.bind(this));
+}
+
+Game.prototype.updateTime = function (timestamp) {
+    if (this.scene=="loading") {
+
+    } else {
+        if (this.last!=null) {
+            var delta = timestamp - this.last
+        }
+        this.last = timestamp;
+    }
 }
