bdc0519066b0f2404fd7409dc0d1f2a8d2466952
dew-x <afigueras@enti.cat>
2015-10-20 17:43:12 +0200

    working blood

--- js/game.js
+++ js/game.js
@@ -5,13 +5,23 @@ function Game(canvasID) {
     this.scene = "loading";
     this.width = 800;
     this.height = 600;
-    this.imageList = [];
+    this.imageList = [
+        "gfx/Backgrounds/CityBackgroundCC01.png",
+        "gfx/Backgrounds/CityTreesCC01.png",
+        
+    ];
+    this.humanList = [
+        "gfx/Characters/CharacterCC01.png",
+        "gfx/Characters/CharacterCC02.png",
+    ];
     this.img = {};
     this.loaded = 0;
     this.data = {
         last: Date.now(),
         blood: 0,
+        humanSpawn: 0,
     };
+    this.humans = [];
     this.secret = "Are you really going to do this? u bstrd!";
     this.units = [
         "Gallon",
@@ -30,8 +40,8 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/town.jpeg",
             onclick: function () {
-
-            }
+                this.moveTo("town");
+            }.bind(this),
         },
         {
             x: this.width*0.1,
@@ -40,8 +50,8 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/minigame.jpg",
             onclick: function () {
-                
-            }
+                this.moveTo("minigame");
+            }.bind(this),
         },
         {
             x: this.width*0.2,
@@ -50,8 +60,8 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/upgrade.gif",
             onclick: function () {
-                
-            }
+                this.moveTo("upgrade");
+            }.bind(this),
         },
         {
             x: this.width*0.7,
@@ -60,8 +70,8 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/achievement.png",
             onclick: function () {
-                
-            }
+                this.moveTo("achievement");
+            }.bind(this),
         },
         {
             x: this.width*0.8,
@@ -70,8 +80,8 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/credits.gif",
             onclick: function () {
-                
-            }
+                this.moveTo("credits");
+            }.bind(this),
         },
         {
             x: this.width*0.9,
@@ -80,11 +90,21 @@ function Game(canvasID) {
             h: this.height*0.1,
             img: "gfx/options.png",
             onclick: function () {
-                
-            }
+                this.moveTo("options");
+            }.bind(this),
         },
     ];
     for (var i=0; i<this.menu.length; ++i) this.imageList.push(this.menu[i].img);
+    for (var i=0; i<this.humanList.length; ++i) this.imageList.push(this.humanList[i]);
+    this.particlesa = [];
+    this.particlesb = [];
+    this.ppb = 50;
+    this.ps = 4;
+}
+
+
+Game.prototype.moveTo = function (target) {
+    console.log(target);
 }
 
 Game.prototype.load = function () {
@@ -183,6 +203,26 @@ Game.prototype.release = function (x,y,id) {
     console.log("M end",x,y,id);
     if (this.scene=="loading" && this.loaded==1) {
         this.scene = "town";
+    } else {
+        for (var i=0; i<this.menu.length; ++i) {
+            if (inside(x,y,this.menu[i].x,this.menu[i].y,this.menu[i].w,this.menu[i].h)) {
+                this.menu[i].onclick();
+                break;
+            }
+        }
+        if (this.scene == "town") {
+            var w = 64;
+            var h = 128;
+            for (var i=this.humans.length-1; i>=0; --i) {
+                var cx = (this.humans[i].x * this.width)-w/2;
+                var cy = (this.humans[i].y * this.height)-h;
+                if (inside(x,y,cx,cy,w,h)) {
+                    console.log(x,y,cx,cy,w,h);
+                    this.kill(this.humans.splice(i,1)[0]);
+                    break;
+                }
+            }
+        }
     }
 }
 Game.prototype.draw = function () {
@@ -204,9 +244,30 @@ Game.prototype.draw = function () {
         this.ctx.textBaseLine = "bottom";
         this.ctx.fillText(this.version,this.width-10,this.height-10)
     } else if (this.scene == "town") {
-        this.ctx.fillStyle="red";
-        this.ctx.fillRect(0,0,this.width, this.height);
+        this.ctx.drawImage(this.img[this.imageList[0]].img,0,0,this.width,this.height);
+        /*this.ctx.fillStyle = "red";
+        this.ctx.fillRect(0, this.height*0.64, this.width, this.height*0.2);*/
+        var w = 64;
+        var h = 128;
+        for (var i=0; i<this.humans.length; ++i) {
+            if (this.humans[i].dir==-1) {
+                this.ctx.save();
+                this.ctx.scale(-1,1);
+                this.ctx.translate(-this.width,0);
+                var x = this.width - (this.humans[i].x * this.width)-w/2;
+                var y = (this.humans[i].y * this.height)-h;
+                this.ctx.drawImage(this.img[this.humanList[this.humans[i].type]].img,x,y,w,h);
+                this.ctx.restore();
+            } else {
+                var x = (this.humans[i].x * this.width)-w/2;
+                var y = (this.humans[i].y * this.height)-h;
+                this.ctx.drawImage(this.img[this.humanList[this.humans[i].type]].img,x,y,w,h);
+            }
+        }
+        this.drawParticles(this.particlesa);
+        this.ctx.drawImage(this.img[this.imageList[1]].img,0,0,this.width,this.height);
         this.drawHeader();
+        this.drawParticles(this.particlesb);
     }
 }
 
@@ -218,7 +279,6 @@ Game.prototype.bloodToText = function () {
     if (exp==0) {
         return this.data.blood.toString()+" "+this.units[exp];
     } else {
-        console.log(this.data.blood,Math.pow(1000,exp))
         return (this.data.blood/Math.pow(1000,exp)).toFixed(1).toString()+" "+this.units[Math.floor(exp)];
     }
 }
@@ -255,8 +315,25 @@ Game.prototype.updateTime = function (timestamp) {
         }
     } else {
         var now = Date.now();
+        if (this.scene=="town") {
+            this.updateHumans(now);
+            this.updateParticles();
+        }
         if (this.data.last<now) {
             this.advanceTo(now);
+        }
+    }
+}
+
+Game.prototype.updateHumans = function (now) { 
+    if (this.humans.length<10 && this.data.humanSpawn<now) {
+        this.spawnHuman();
+        this.data.humanSpawn = now + 5;
+    }
+    for (var i=this.humans.length-1; i>=0; --i) {
+        this.humans[i].x += this.humans[i].dir * this.humans[i].speed;
+        if ((this.humans[i].dir==-1 && this.humans[i].x<-0.2) || (this.humans[i].dir==1 && this.humans[i].x>1.2)) {
+            this.humans.splice(i,1);
         }
     }
 }
@@ -267,4 +344,82 @@ Game.prototype.advanceTo = function (timestamp) {
     this.data.blood += Math.ceil(delta);
     var encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.data), this.secret).toString();
     localStorage.setItem("data",encrypted);
+}
+
+Game.prototype.updateParticles = function () {
+    var speed = 0.09;
+    var tx = 400;
+    var ty = 30;
+    var now = Date.now();
+    for (var i=this.particlesa.length-1; i>=0; --i) {
+        this.particlesa[i].vx += this.particlesa[i].ax*speed;
+        this.particlesa[i].vy += this.particlesa[i].ay*speed;
+        this.particlesa[i].x += this.particlesa[i].vx*speed;
+        this.particlesa[i].y += this.particlesa[i].vy*speed;
+        if (this.particlesa[i].y>this.particlesa[i].my) {
+            var part = this.particlesa.splice(i,1)[0];
+            this.particlesb.push(part);
+        }
+    }
+    for (var i=this.particlesb.length-1; i>=0; --i) {
+        var alpha = (now-this.particlesb[i].now)/1500;
+        var dist = dist2d(this.particlesb[i].x,this.particlesb[i].y,tx,ty);
+        if (dist>10) {
+            this.particlesb[i].x += (tx-this.particlesb[i].x+this.particlesb[i].vx)/dist*alpha;
+            this.particlesb[i].y += (ty-this.particlesb[i].y)/dist*alpha;
+        } else {
+            this.particlesb.splice(i,1)[0];
+        }
+    }
+}
+
+Game.prototype.spawnHuman = function () {
+    var human = {
+        x: ((Math.random()<0.5)?-0.2:1.2),
+        y: map(Math.random(),0,1,0.64,0.84),
+        type: randomInt(0,this.humanList.length),
+        speed: map(Math.random(),0,1,0.001,0.003),
+        dir: 0,
+    }
+    human.dir = (human.x==-0.2)?1:-1;
+    this.humans.push(human);
+    this.humans.sort(function (a, b) {
+        if (a.y > b.y) {
+            return 1;
+        }
+        if (a.y < b.y) {
+            return -1;
+        }
+        // a must be equal to b
+        return 0;
+    });
+}
+
+Game.prototype.kill = function (dead) {
+    var h = 128;
+    var x = (dead.x * this.width);
+    var y = (dead.y * this.height)-h/2;
+    var miny = (dead.y * this.height)-h/4;
+    var maxy = (dead.y * this.height)+h/4;
+    var now = Date.now();
+    for (var i=0; i<this.ppb; ++i) {
+        var part = {
+            x: x,
+            y: y,
+            vx: map(Math.random(),0,1,-10,10),
+            vy: map(Math.random(),0,1,-10,10),
+            my: map(Math.random(),0,1,miny,maxy),
+            ax: 0,
+            ay: 10,
+            now: now,
+        }
+        this.particlesa.push(part);
+    }
+}
+
+Game.prototype.drawParticles = function (particles) {
+    this.ctx.fillStyle = "red";
+    for (var i=particles.length-1; i>=0; --i) {
+        this.ctx.fillRect(particles[i].x,particles[i].y,this.ps,this.ps);
+    }
 }
--- js/lib.js
+++ js/lib.js
@@ -13,3 +13,19 @@ function getPosition(element) {
 function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
 }
+
+function inside(x,y,x0,y0,w,h) {
+    return x>=x0 && x<=x0+w && y>=y0 && y<=y0+h;
+}
+
+function map(val,min,max,a,b) {
+    return (val-min)/(max-min)*(b-a)+a;
+}
+
+function randomInt(a,b) {
+    return Math.floor(Math.random()*(b-a)+a);
+}
+
+function dist2d(x,y,x0,y0) {
+    return Math.sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0));
+}
